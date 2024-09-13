1c93152be879e61d50fb531f66906dadba78c911
Elsa Gas Wikström <eg223ps@student.lnu.se>
2024-09-13 13:32:19 +0200

    Updated emailValidator

--- src/emailValidator.js
+++ src/emailValidator.js
@@ -2,7 +2,21 @@ export class EmailValidator {
   constructor() {}
 
   validate(email){
+    if (!email) {
+      return { isValid: false, error: "Email is required" }
+    }
+
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
-    return emailRegex.test(email)
+    const forbiddenCharsRegex = /[^\w@.-]/
+
+    if (forbiddenCharsRegex.test(email)) {
+      return { isValid: false, error: "Email contains forbidden characters "}
+    }
+
+    if (!emailRegex.test(email)) {
+      return { isValid: false, error: "Email is not valid." }
+    }
+
+    return { isValid: true, error: null }
   }
 }
--- src/index.js
+++ src/index.js
@@ -3,3 +3,5 @@ import { EmailValidator } from './emailValidator.js'
 const emailValidator = new EmailValidator
 
 console.log(emailValidator.validate('elsa@gmail.com'))
+console.log(emailValidator.validate(''))
+console.log(emailValidator.validate('elsagmail.com'))
