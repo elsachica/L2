0dc62b8529f74bdf603c183fff468f1a879e2309
Elsa Gas Wikström <eg223ps@student.lnu.se>
2024-09-16 09:13:30 +0200

    Included swe and US postal codes in postalCodeValidator

--- src/index.js
+++ src/index.js
@@ -7,11 +7,13 @@ const postalCodeValidator = new PostalCodeValidator
 // Test
 
 // EmailValidator
-// console.log(emailValidator.validate('elsa@gmail.com'))
+// console.log(emailValidator.validate('example@gmail.com'))
 // console.log(emailValidator.validate(''))
-// console.log(emailValidator.validate('elsagmail.com'))
+// console.log(emailValidator.validate('examplegmail.com'))
 
 // PostalCodeValidator
-console.log(postalCodeValidator.validate('12345'))
-console.log(postalCodeValidator.validate('12345-6789'))
-console.log(postalCodeValidator.validate('1234'))
+console.log(postalCodeValidator.validate('12345')) // USA and Sweden
+console.log(postalCodeValidator.validate('12345-6789')) // USA
+console.log(postalCodeValidator.validate('123 45')) // Sweden
+console.log(postalCodeValidator.validate('1234')) // false
+
--- src/postalCodeValidator.js
+++ src/postalCodeValidator.js
@@ -2,7 +2,19 @@ export class PostalCodeValidator {
   constructor() {}
 
   validate(postalCode) {
-    const postalCodeRegex = /^\d{5}(-\d{4})?$/
-    return postalCodeRegex.test(postalCode)
+    // US-format: 5 siffror eller 5 siffror följt av bindestreck och 4 siffror
+    const usPostalCodeRegex = /^\d{5}(-\d{4})?$/
+
+    // Svenskt format: 5 siffror med eller utan mellanslag
+    const swePostalCodeRegex = /^\d{3}\s?\d{2}$/
+
+
+    if (usPostalCodeRegex.test(postalCode)) {
+      return { isValid: true, coutnry: 'USA' }
+    } else if (swePostalCodeRegex.test(postalCode)) {
+      return { isValid: true, country: 'Sweden' }
+    } else {
+      return { isValid: false, message: 'Not a valid postal code'}
+    }
   }
 }
