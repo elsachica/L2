0ce722ad4ef26573b5ff2ee9456cc3074bf603e6
Elsa Gas Wikström <eg223ps@student.lnu.se>
2024-09-25 13:12:43 +0200

    Comments to explain regex in PhoneNUmberValidator

--- src/validators/phoneNumberValidator.js
+++ src/validators/phoneNumberValidator.js
@@ -15,10 +15,10 @@ export class PhoneNumberValidator {
       return { isValid: false, error: "Phone number is required." }
     }
 
-    const swePhoneRegex = /^(?:\+46|0)\d{9}$/
-    const usPhoneRegex = /^(?:\+1\s?)?\d{10}$/
-    const ukPhoneRegex = /^(?:\+44|0)\d{10}$/
-    const espPhoneRegex = /^(?:\+34\s?|0)?[67]\d{8}$/
+    const swePhoneRegex = /^(?:\+46|0)\d{9}$/ // Sweden phone number format: 0701234567 or +46701234567
+    const usPhoneRegex = /^(?:\+1\s?)?\d{10}$/ // US phone number format: 1234567890 or +11234567890
+    const ukPhoneRegex = /^(?:\+44|0)\d{10}$/ // UK phone number format: 07123456789 or +447123456789
+    const espPhoneRegex = /^(?:\+34\s?|0)?[67]\d{8}$/ // Spain phone number format: 612345678 or +34612345678
 
     if (swePhoneRegex.test(phoneNumber)) {
       return { isValid: true, format: "Swedish" }
